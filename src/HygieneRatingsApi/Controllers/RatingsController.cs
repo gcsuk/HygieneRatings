7933d5f9b7348c6b197b25d9e3b6e33c54d8bb73
Rob King <rob.king@bedegaming.com>
2016-11-30 15:42:09 +0000

    Added post code / restaurant name lookup

--- src/HygieneRatingsApi/Controllers/RatingsController.cs
+++ src/HygieneRatingsApi/Controllers/RatingsController.cs
@@ -12,10 +12,51 @@ namespace HygieneRatings.Controllers
     public class RatingsController : Controller
     {
         private readonly IRatingsService _ratingsService;
+        private readonly IGeolocationService _geolocationService;
 
-        public RatingsController(IRatingsService ratingsService)
+        public RatingsController(IRatingsService ratingsService, IGeolocationService geolocationService)
         {
             _ratingsService = ratingsService;
+            _geolocationService = geolocationService;
+        }
+
+        /// <remarks>Returns all matching businesses for specified name and location</remarks>
+        /// <response code="200">Returns the ratings</response>
+        /// <response code="404">No ratings were found for the specified name and co-ordinates</response>
+        /// <response code="500">Unexpected error</response>
+        /// <returns>A list of rating objects</returns>
+        [HttpGet("{name}/{postCode}")]
+        [ProducesResponseType(typeof(IEnumerable<RatingsVm>), 200)]
+        public async Task<IActionResult> Get(string name, string postCode)
+        {
+            var geolocationResults = await _geolocationService.GetCoordinates(postCode);
+
+            if (geolocationResults == null || !geolocationResults.Results.Any())
+            {
+                return NotFound();
+            }
+
+            var geolocation = geolocationResults.Results.First().Geometry.Location;
+
+            var results = await _ratingsService.GetRatings(name, geolocation.Latitude, geolocation.Longitude);
+
+            if (results == null || !results.Establishments.Any())
+            {
+                return NotFound();
+            }
+
+            return
+                Ok(new List<RatingsVm>(
+                    results.Establishments.Select(
+                        e =>
+                            new RatingsVm
+                            {
+                                BusinessName = e.BusinessName,
+                                BusinessPostCode = e.PostCode,
+                                Rating = e.RatingValue,
+                                RatingDate = e.RatingDate,
+                                RatingKey = e.RatingKey
+                            })));
         }
 
         /// <remarks>Returns all matching businesses for specified name and location</remarks>
